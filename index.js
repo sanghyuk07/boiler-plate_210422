5b840cc17a8a18158ec24b48975421f817c759a2
sanghyuk07 <74354435+sanghyuk07@users.noreply.github.com>
2021-04-24 23:01:10 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express')
 const app = express()
 const port = 5001
 
-const mongoose =require('mongoose')
-mongoose.connect('mongodb+srv://sanghyuk:gmlsend134@bolierplate.ezt9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const mongoose =require('mongoose');
+
+const config=require('./config/key');
+const { User } = require("./models/User");
+
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
 }).then(()=> console.log('Mongdb Connected..'))
 .catch(err =>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!, 무야호, MOOYAHO')
 })
 
 app.listen(port, () => {
-  console.log(`✅app listening at http://localhost:${port}`)
+  console.log(`✅app listening at http://localhost:${port}!`)
 })
